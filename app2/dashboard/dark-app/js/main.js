51f18105a1aa3f76cffd20397399b60d8541abe7
Memo Jimenez <memojl08@gmail.com>
2020-10-31 12:25:24 -0600

    Aplication App

---
+++ app2/dashboard/dark-app/js/main.js
@@ -0,0 +1,97 @@
+const loggedInLinks = document.querySelectorAll(".dashboard");
+const loggedOutLinks = document.querySelectorAll(".login-page");
+
+
+const loginCheck = (user) => {
+  if (user) {
+    loggedInLinks.forEach((link) => (link.style.display = "block"));
+    loggedOutLinks.forEach((link) => (link.style.display = "none"));
+  } else {
+    loggedInLinks.forEach((link) => (link.style.display = "none"));
+    loggedOutLinks.forEach((link) => (link.style.display = "block"));
+  }
+};
+
+
+// Logout
+const logout = document.querySelector("#logout");
+
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("signup out");
+  });
+});
+
+// SingIn
+const signInForm = document.querySelector("#login-form");
+
+signInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signInForm["login-email"].value;
+  const password = signInForm["login-password"].value;
+
+  // Authenticate the User
+  auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+    // clear the form
+    signInForm.reset();
+    // close the modal
+    //$("#signinModal").modal("hide");
+  });
+});
+
+// Posts
+const postList = document.querySelector(".posts");
+const setupPosts = (data) => {
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const post = doc.data();
+      const li = `
+      <li class="list-group-item list-group-item-action">
+        <h5>${post.title}</h5>
+        <p>${post.content}</p>
+      </li>
+    `;
+      html += li;
+    });
+    postList.innerHTML = html;
+  } else {
+    postList.innerHTML = '<h4 class="text-white">Login to See Posts (Git/master Version)</h4>';
+  }
+};
+
+// events
+// list for auth state changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log("signin");
+    fs.collection("posts").get().then((snapshot) => {
+        //setupPosts(snapshot.docs);
+        loginCheck(user);
+        setupPosts(snapshot.docs);
+      });
+  } else {
+    console.log("signout");
+    setupPosts([]);
+    loginCheck(user);
+  }
+});
+
+// Login with Google
+const googleButton = document.querySelector("#googleLogin");
+
+googleButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  signInForm.reset();
+  //$("#signinModal").modal("hide");
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider).then((result) => {
+    console.log(result);
+    console.log("google sign in");
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});
