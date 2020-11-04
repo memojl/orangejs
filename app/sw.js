98da5224d8dbfd25e180dbf1486dd580483adddf
Memo Jimenez <memojl08@gmail.com>
2020-11-04 13:25:49 -0600

    update 04.11.2020

--- app/sw.js
+++ app/sw.js
@@ -20,7 +20,7 @@ self.addEventListener('fetch', function(event) {//console.log(event.request.url)
 		} else {
 		  return fetch(event.request).then(function(res) {
 			return caches.open('dynamic').then(function(cache) {
-			  //cache.put(event.request.url, res.clone()).then(()=>{cache.delete('/MisSitios/orangejs/app/admin/');});
+			  //cache.put(event.request.url, res.clone()).then(()=>{cache.delete('/MisSitios/orangejs/app/');});
 			  cache.put(event.request.url, res.clone()).then(()=>{cache.delete(event.request.url);});
 			  return res;
 			});
