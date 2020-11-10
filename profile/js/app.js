1aa36943791d6dcf895d81e903190a45c2de4596
Memo Jimenez <memojl08@gmail.com>
2020-11-09 23:57:11 -0600

    update 09.11.2020

--- profile/js/app.js
+++ profile/js/app.js
@@ -13,7 +13,13 @@ fetch(url_json).then(response => response.json()).then(data => {
   var val=Object.values(data);
   for(i=0;i<val.length;i++){reg=val[i];
     if(reg.profile==perfil){console.log(reg)
+      document.getElementById('nom').innerHTML=reg.ID;
+      document.getElementById('cov').innerHTML=reg.cover;
+      document.getElementById('pro').innerHTML=reg.profile;
       document.getElementById('nom').innerHTML=reg.nombre;
+      document.getElementById('emp').innerHTML=reg.empresa;
+      document.getElementById('pue').innerHTML=reg.puesto;
+      document.getElementById('ema').innerHTML=reg.email;
     }
   }
   /*
