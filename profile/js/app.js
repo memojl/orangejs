ae8e9a3ae6c188745f94eab920a38528bfd46069
Memo Jimenez <memojl08@gmail.com>
2020-11-10 16:53:59 -0600

    update frontend 10112020

--- profile/js/app.js
+++ profile/js/app.js
@@ -33,9 +33,13 @@ fetch(url_json).then(response => response.json()).then(data => {
   for(i=0;i<val.length;i++){reg=val[i];
     if(reg.profile==perfil){console.log(reg)
       document.getElementById('idp').innerHTML=reg.ID;
-      document.getElementById('cov').innerHTML='<img src="../app/assets/img/photos/'+reg.cover+'">';
+      document.getElementById('cov').innerHTML='<div class="img" ng-style="{&#39;background&#39;:view.avatar?&#39;url(&#39;+view.avatar+&#39;)&#39;:&#39;none&#39;}" style="background: url(../app/assets/img/photos/'+reg.cover+');"></div>';
       document.getElementById('prof').innerHTML=reg.profile;
       document.getElementById('nom').innerHTML=reg.nombre;
+      document.getElementById('btn-cell').innerHTML='<a href="tel:'+reg.cell+'"><i class="icon-phone dynamicTextColor"></i><small class="dynamicTextColor">Llamar</small></a>';
+      document.getElementById('btn-email').innerHTML='<a href="mailto:'+reg.email+'?subject=Desde%20Mi%20Tarjeta&body=" target="_newEmail""><i class="icon-send dynamicTextColor"></i><small class="dynamicTextColor">Email</small></a>';
+
+      document.getElementById('bio').innerHTML=reg.descripcion;
       document.getElementById('emp').innerHTML=reg.empresa;
       document.getElementById('pue').innerHTML=reg.puesto;
     }
