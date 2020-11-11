9fc392479b1ca51baacaadf3b5c58abae6a62f9b
Memo Jimenez <memojl08@gmail.com>
2020-11-11 08:49:05 -0600

    pre-update dominio

--- profile/js/app2.js
+++ profile/js/app2.js
@@ -1,10 +1,8 @@
 var url_json = 'https://crud-ce022.firebaseio.com/vcard_vcard/-MLBBSCOdiImZ-yxdJmx.json';
-console.log(url_json);
-
-fetch(url_json).then(res => res.json()).then(data => {getJson(data);});
-
 var file=getJson();
 console.log(file);
+
+fetch(url_json).then(res => res.json()).then(data => {getJson(data);});
 
 function getJson(data){
   //const nJson=data;//
