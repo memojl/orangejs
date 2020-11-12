0b1a4b0382fdb0a253377b26d78673c546c7dd5c
Memo Jimenez <memojl08@gmail.com>
2020-11-12 11:14:12 -0600

    pre-update 12112020

--- app/bloques/crear_db.php
+++ app/bloques/crear_db.php
@@ -18,16 +18,25 @@ curl_close($ch);
 }
 
 /*vcard_vcard */
+
 $ramat='vcard_vcard';
 $data='';
 $data=array(
-    '{"ID":"1","cover":"foto.png","profile":"rforesta","logo":"","nombre":"Rodrigo Foresta","descripcion":"","puesto":"Manager","empresa":"Billnex","tel":"","tel_ofi":"","cell":"+54 9 3534 19 6770","email":"rodrigo.foresta@thebillnex.com","web":"https://www.thebillnex.com","fb":"","tw":"","lk":"#","ins":"https://www.instagram.com/billnex","f_create":"19/08/2020 10:38","f_update":"2020-08-30 12:13:28","vcard":"1","user":"admin","visible":"1"}',
-    '{"ID":"2","cover":"foto.png","profile":"jparra","logo":"","nombre":"Juan Parra","descripcion":"","puesto":"Manager","empresa":"Billnex","tel":"","tel_ofi":"","cell":"+1(754)210-0433","email":"juan.parra@thebillnex.com","web":"https://www.thebillnex.com","fb":"","tw":"","lk":"#","ins":"https://www.instagram.com/billnex","f_create":"22/08/2020 17:04","f_update":"2020-08-30 12:13:42","vcard":"1","user":"admin","visible":"1"}',
-    '{"ID":"4","cover":"foto_capital.png","profile":"pbetancourt","logo":"","nombre":"Ponciano Betancourt","descripcion":"","puesto":"Manager","empresa":"Capital","tel":"","tel_ofi":"","cell":"442 347 0504","email":"pbetancourt@capitalsft.com","web":"https://www.capitalsft.com","fb":"","tw":"","lk":"https://www.linkedin.com/company/13990038/admin/","ins":"","f_create":"22/08/2020 21:39","f_update":"2020-08-30 13:17:20","vcard":"1","user":"admin","visible":"1"}',
-    '{"ID":"5","cover":"foto_capital.png","profile":"memojl","logo":"","nombre":"Guillermo Jimenez Lopez","descripcion":"","puesto":"Programador","empresa":"Multiportal","tel":"","tel_ofi":"","cell":"4426002842","email":"multiportal@outlook.com","web":"http://multiportal.com.mx","fb":"","tw":"","lk":"https://www.linkedin.com/","ins":"https://www.instagram.com/","f_create":"2020-08-30 11:05:44","f_update":"2020-08-30 13:17:34","vcard":"1","user":"admin","visible":"1"}');
+    '{"ID":"1","cell":"+54 9 3534 19 6770","cover":"sinfoto.png","descripcion":"","email":"rodrigo.foresta@thebillnex.com","empresa":"Billnex","f_create":"19/08/2020 10:38","f_update":"2020-08-30 12:13:28","fb":"","idemp":3,"ins":"https://www.instagram.com/billnex","lk":"#","logo":"","nombre":"Rodrigo Foresta","profile":"rforesta","puesto":"Manager","tel":"","tel_ofi":"","tw":"","uid":"llrPAIN2I6YnE5RE88dsHAPNejl1","user":"admin","vcard":"1","visible":"1","web":"https://www.thebillnex.com"}',
+    '{"ID":"2","cell":"+1(754)210-0433","cover":"avatar-0.jpg","descripcion":"Administración de Marketing","email":"juan.parra@thebillnex.com","empresa":"Billnex","f_create":"22/08/2020 17:04","f_update":"2020-08-30 12:13:42","fb":"","idemp":3,"ins":"https://www.instagram.com/billnex","lk":"#","logo":"","nombre":"Juana Parra","profile":"jparra","puesto":"Manager","tel":"","tel_ofi":"","tw":"","uid":"2RPUIugJ8nfQzPWqJ8htxjSr2iH3","user":"admin","vcard":"1","visible":"1","web":"https://www.thebillnex.com"}',
+    '{"ID":"4","cell":"442 347 0504","cover":"avatar-1.jpg","descripcion":"","email":"pbetancourt@capitalsft.com","empresa":"Capital","f_create":"22/08/2020 21:39","f_update":"2020-08-30 13:17:20","fb":"","idemp":2,"ins":"","lk":"https://www.linkedin.com/company/13990038/admin/","logo":"","nombre":"Ponciano Betancourt","profile":"pbetancourt","puesto":"Manager","tel":"","tel_ofi":"","tw":"","uid":"2RPUIugJ8nfQzPWqJ8htxjSr2iH3","user":"admin","vcard":"1","visible":"1","web":"https://www.capitalsft.com"}',
+    '{"ID":"5","cell":"4426002842","cover":"darth.jpg","descripcion":"Desarrollo de Paginas Web y Marketing Digital","email":"multiportal@outlook.com","empresa":"Multiportal","f_create":"2020-08-30 11:05:44","f_update":"2020-08-30 13:17:34","fb":"https://facebook.com/","idemp":1,"ins":"https://www.instagram.com/","lk":"https://www.linkedin.com/","logo":"","nombre":"Guillermo Jimenez Lopez","profile":"memojl","puesto":"Programador","tel":"","tel_ofi":"","tw":"","uid":"JjxZi4gu7qOzbAU2hk7fChlm6PC2","user":"admin","vcard":"1","visible":"1","web":"http://multiportal.com.mx"}'
+);
 for($i=0;$i<count($data);$i++){insertar_db($data[$i],$ramat);}
 echo '<div>Total de Registros: ['.$i.'] en '.$ramat.'</div>';
+
 /*vcard_vcard_empresas */
 $ramat='vcard_vcard_empresas';
-$data='{"ID":"1","cover":"multiportal.jpg","empresa":"Multiportal","web":"http://multiportal.com.mx","tel":"442602842","email":"multiportal@outlook.com","ID_user":"1","user":"admin","f_create":"2020-09-05","f_update":"2020-09-05","visible":"1"}';
-insertar_db($data,$ramat);
+$data='';
+$data=array(
+    '{"ID":"1","bg_color":"#000","cover":"multiportal.jpg","empresa":"Multiportal","uid":"JjxZi4gu7qOzbAU2hk7fChlm6PC2","f_create":"2020-09-05","f_update":"2020-09-05","visible":"1"}',
+    '{"ID":"2","bg_color":"#333","cover":"capital.jpg","empresa":"Capital","uid":"2RPUIugJ8nfQzPWqJ8htxjSr2iH3","f_create":"2020-09-05","f_update":"2020-09-05","visible":"1"}',
+    '{"ID":"3","bg_color":"#f8971d","cover":"billnex.jpg","empresa":"Billnex","uid":"2RPUIugJ8nfQzPWqJ8htxjSr2iH3","f_create":"2020-09-05","f_update":"2020-09-05","visible":"1"}'
+);
+for($i=0;$i<count($data);$i++){insertar_db($data[$i],$ramat);}
+echo '<div>Total de Registros: ['.$i.'] en '.$ramat.'</div>';
--- profile/js/app.js
+++ profile/js/app.js
@@ -26,11 +26,11 @@ var page_url = dominio+path_root;
 console.log('page_url='+page_url);
 var perfil = filename();
 console.log(perfil);
-var url_json = 'https://crud-ce022.firebaseio.com/vcard_vcard.json';
-console.log(url_json);
+var url_json_vcard = 'https://crud-ce022.firebaseio.com/vcard_vcard.json';
+console.log(url_json_vcard);
 //const shareLink = document.querySelectorAll("#shareLink");
 
-fetch(url_json).then(response => response.json()).then(data => {
+fetch(url_json_vcard).then(response => response.json()).then(data => {
   var val=Object.values(data);
   for(i=0;i<val.length;i++){reg=val[i];
     if(reg.profile==perfil){console.log(reg)
@@ -48,7 +48,7 @@ fetch(url_json).then(response => response.json()).then(data => {
       //document.getElementById('tel').innerHTML=reg.tel;
       document.getElementById('mailto').innerHTML=reg.email;
       document.getElementById('mailto').href='mailto:'+reg.email;
-      document.getElementById('empresa').innerHTML=reg.empresa;
+      //document.getElementById('empresa').innerHTML=reg.empresa;
       document.getElementById('web').innerHTML=reg.web;
       document.getElementById('web').href=reg.web;
 
@@ -109,6 +109,7 @@ fetch(url_json).then(response => response.json()).then(data => {
       }else{var lk='';}
       document.getElementById('social-media').innerHTML=fb+tw+ins+lk;
 
+      empresa(reg.idemp);
     }
   }
   /*
@@ -120,6 +121,24 @@ fetch(url_json).then(response => response.json()).then(data => {
   var regis=val.filter(parentId(perfil));
   console.log(regis[0]);*/
 });
+
+function empresa(idemp){
+  var url_json_empresa = 'https://crud-ce022.firebaseio.com/vcard_vcard_empresas.json';
+  console.log(url_json_empresa);
+  fetch(url_json_empresa).then(response => response.json()).then(data => {
+    var val=Object.values(data);
+    for(i=0;i<val.length;i++){reg=val[i];
+      //if(reg.uid==userId){console.log(reg)
+        if(reg.ID==idemp){console.log(reg)
+          document.getElementById('bg-fondo').style.backgroundColor=reg.bg_color;
+          //document.getElementById('prime').style.backgroundColor=reg.bg_color;
+          document.getElementById('empresa').innerHTML=reg.empresa;
+          //document.getElementById('logo').innerHTML=reg.cover;
+        }
+      //}
+    }
+  });
+}
 
 function filename(){
   var rutaAbsoluta = self.location.href;
